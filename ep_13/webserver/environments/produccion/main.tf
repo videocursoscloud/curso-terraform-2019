ab2c65cb1283158b9d3203a9ec6d60dcfd6d66b9
Your Name <you@example.com>
2019-01-27 06:12:21 -0800

    changes

--- ep_13/webserver/environments/produccion/main.tf
+++ ep_13/webserver/environments/produccion/main.tf
@@ -1,5 +1,5 @@
 module "webserver" {
-  source = "github.com/videocursoscloud/terraform-module-ec2-with-eip?ref=v1.0.3"
+  source = "github.com/videocursoscloud/terraform-module-ec2-with-eip?ref=v1.0.2"
   ami = "ami-00035f41c82244dab"
   instance_type = "t2.micro"
   key_name = "microserver"
